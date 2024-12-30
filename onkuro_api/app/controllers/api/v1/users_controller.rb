1b4c0dab188efef3b0f099d0afc929b774644d4d
Fumiyoshi.K <fumi@MacBook-Air.local>
2024-12-30 14:55:40 +0900

    User登録APIの作成

--- onkuro_api/app/controllers/api/v1/users_controller.rb
+++ onkuro_api/app/controllers/api/v1/users_controller.rb
@@ -2,10 +2,10 @@ class Api::V1::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if user.save
-      render json: @user, status: :created, location: @user
+    if @user.save
+      render json: @user, status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: {error: @user.errors.fullmessages }, status: :unprocessable_entity
     end
   end
 
